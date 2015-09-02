1676813ca4cb19ae740921dd3c5abfa69d64ba42
Marcus Albertsson <marcus.arubertoson@gmail.com>
2015-09-02 12:43:33 +0200

    add SelectionMask class for working with selection masks in Maya

--- mampy/utils.py
+++ mampy/utils.py
@@ -6,6 +6,7 @@ import collections
 
 from maya import cmds
 import maya.OpenMaya as oldapi
+from maya.OpenMaya import MGlobal as mgl
 
 from PySide import QtGui
 from mampy.packages.mvp import Viewport
@@ -13,7 +14,8 @@ from mampy.packages.contextlib2 import ContextDecorator, contextmanager
 
 
 __all__ = ['history_chunk', 'select_keep', 'object_selection_mode',
-           'get_object_under_cursor', 'get_objects_in_view', 'OptionVar']
+           'get_object_under_cursor', 'get_objects_in_view',
+           'OptionVar', 'SelectionMask']
 
 
 class history_chunk(ContextDecorator):
@@ -227,6 +229,134 @@ class OptionVarList(tuple):
             )
 
 
-if __name__ == '__main__':
+class SelectionMask(object):
+    """
+    Selection mask class for accessing and chaning selection mask
+    information.
+    """
 
+    (kSelectObjectMode,
+     kSelectComponentMode,
+     kSelectRootModem,
+     kSelectLeafMode,
+     kSelectTemplateMode) = range(5)
+
+    def __new__(cls, mask=None):
+        cls = object.__new__(cls, mask)
+        for a in oldapi.MSelectionMask.__dict__:
+            if a.startswith('kSelect'):
+                setattr(cls, a, oldapi.MSelectionMask.__dict__[a])
+        return cls
+
+    def __init__(self, mask=None):
+        self._mask = mask or oldapi.MSelectionMask()
+
+    def __repr__(self):
+        return '{}({})'.format(self.__class__.__name__, list(self))
+
+    def __str__(self):
+        return '{}'.format(self.typestr)
+
+    def __iter__(self):
+        return iter(self.get_active_masks(local=True))
+
+    def __contains__(self, value):
+        return self._mask.intersects(value)
+
+    @property
+    def mask(self):
+        return self._mask
+
+    @property
+    def mode(self):
+        return mgl.selectionMode()
+
+    @property
+    def typestr(self):
+        return self.get_active_masks(internal=False, local=True)
+
+    @classmethod
+    def active(cls):
+        """
+        Return active selection mask.
+        """
+        mask = {
+            mgl.kSelectObjectMode: mgl.objectSelectionMask(),
+            mgl.kSelectComponentMode: mgl.componentSelectionMask(),
+        }[mgl.selectionMode()]
+        return cls(mask)
+
+    @staticmethod
+    def set_mode(kmode):
+        """
+        Set selection mode.
+
+        :param kmode: internal selection mode pointer.
+        """
+        mgl.setSelectionMode(kmode)
+
+    def add(self, other):
+        """
+        Add mask(s) to the current :class:`SelectionMask` object.
+        """
+        if type(other) in [list, tuple, set]:
+            for o in other:
+                self._mask.addMask(o)
+        else:
+            self._mask.addMask(other)
+        self.update()
+
+    def set_mask(self, other):
+        """
+        Set active mask to given ``OpenMaya.MSelectionMask`` or
+        internal int pointer.
+        """
+        self._mask.setMask(other)
+        self.update()
+
+    def get_active_masks(self, internal=True, local=False):
+        """
+        Construct and return active mask set.
+
+        :param internal: wether rtype is internal int pointers or
+            string attributes.
+        :param local: wether to look for masks in local ``SelectionMask``
+            object or global ``OpenMaya.MGlobal`` mask.
+        :rtype: ``set``
+        """
+        active = set()
+        for m in self.__dict__:
+            if not m.startswith('kSelect'):
+                continue
+
+            if local:
+                space = self
+            else:
+                space = self.active()
+
+            if self.__dict__[m] in space:
+                if internal:
+                    active.add(self.__dict__[m])
+                else:
+                    active.add(m)
+        return active
+
+    def clear(self):
+        """
+        Empty mask by creating new one and overriding it.
+        """
+        self._mask = oldapi.MSelectionMask()
+        self.update()
+
+    def update(self):
+        """
+        Set to be the active mask in Maya.
+        """
+        if self.mode == mgl.kSelectComponentMode:
+            mgl.setComponentSelectionMask(self._mask)
+        elif self.mode == mgl.kSelectObjectMode:
+            mgl.setObjectSelectionMask(self._mask)
+
+
+if __name__ == '__main__':
     print get_object_under_cursor()
