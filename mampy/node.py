48a046dd2a902a353ae398f8006a536052c302fc
Marcus Albertsson <marcus.arubertoson@gmail.com>
2016-01-01 20:04:38 +0100

    Various fixes and new functionality added
    	fix bugs  return values in DagNode class
    	added new class Transforms

--- mampy/node.py
+++ mampy/node.py
@@ -6,10 +6,15 @@
 
     Classes for Maya node types.
 
-    .. todo:: module docstring.
+    .. todo::
+        * module docstring.
+        * rewrite as plugin? As it stands the script collection can't
+        be used with certain api functions.
 
 """
+import math
 import logging
+import collections
 
 import maya.cmds as cmds
 import maya.OpenMaya as oldapi
@@ -186,28 +191,71 @@ class DagNode(object):
     def from_object(cls, mobject):
         return cls(api.MDagPath.getAPathTo(mobject))
 
-    def get_shape(self, index=0):
+    def set_parent(self, other, **kwargs):
         """
-        Return shape at given index. If index is not given return first
-        in list.
-        """
-        shape = self.__class__(self._dagpath.extendToShape(index))
-        self._dagpath = self._dagnode.dagPath()
-        return shape
+        .. todo::
 
-    def get_parent(self, index=0):
+            * try to avoid using cmds, find a way to update matrix after
+            using addChild.
+
+        .. note::
+            Don't know if its possible with api commands since you would have
+            to write an undo function of some kind.
         """
-        Return parent at given index. If index is not given return
-        first in list.
-        """
-        return self.from_object(self._dagnode.parent(index))
+        if isinstance(other, basestring):
+            n = cmds.parent(self.name, other, **kwargs)[0]
+        else:
+            n = cmds.parent(self.name, other.name, **kwargs)[0]
+        self = self.__class__(n)
+        return self
 
     def get_child(self, index=0):
         """
         Return child at given index. If index is not given return first
         in list.
         """
-        return self.from_object(self._dagnode.child(index))
+        try:
+            return self.from_object(self._dagnode.child(index))
+        except RuntimeError:
+            return None
+
+    def get_parent(self, index=0):
+        """
+        Return parent at given index. If index is not given return
+        first in list.
+        """
+        try:
+            return self.from_object(self._dagnode.parent(index))
+        except RuntimeError:
+            return None
+
+    def get_shape(self, index=0):
+        """
+        Return shape at given index. If index is not given return first
+        in list.
+        """
+        try:
+            shape = self.__class__(self._dagpath.extendToShape(index))
+            self._dagpath = self._dagnode.dagPath()
+            return shape
+        except RuntimeError:
+            return None
+
+    def get_transform(self):
+        """
+        Return the closest transform node from the given dagpath.
+        """
+        return Transform(self._dagpath.getAPathTo(self._dagpath.transform()))
+
+    def is_child_of(self, other):
+        return self._dagnode.hasParent(other.node)
+
+    def is_parent_of(self, other):
+        return self._dagnode.hasChild(other.node)
+
+    def is_root(self):
+        p = self.get_parent()
+        return p is None or p.type == api.MFn.kWorld
 
     def iterchildren(self):
         """
@@ -234,7 +282,6 @@ class Camera(DagNode):
 
         s = oldapi.MSelectionList(); s.add(self.fullpath)
         dp = oldapi.MDagPath(); s.getDagPath(0, dp)
-
         self._mfncam = oldapi.MFnCamera(dp)
 
     def get_view_direction(self, space=api.MSpace.kWorld):
@@ -250,5 +297,72 @@ class Camera(DagNode):
         # return api.MVector(self._mfncam.upDirection(space))
 
 
+class Transform(DagNode):
+    """
+    Wraps ``OpenMaya.MFnTransform``.
+    """
+
+    Transforms = collections.namedtuple('Transforms', 'translate rotate scale')
+
+    def __init__(self, dagpath):
+        super(Transform, self).__init__(dagpath)
+        self._mfntrans = api.MFnTransform(dagpath)
+
+    def get_transforms(self):
+        return self.Transforms(
+            self.get_translation(),
+            self.get_rotate(),
+            self.get_scale(),
+        )
+
+    def get_rotate_pivot(self, space=api.MSpace.kWorld):
+        return api.MVector(self._mfntrans.rotatePivot(space))
+
+    def get_scale_pivot(self, space=api.MSpace.kWorld):
+        return api.MVector(self._mfntrans.scalePivot(space))
+
+    def get_translation(self, space=api.MSpace.kWorld):
+        return self._mfntrans.translation(space)
+
+    def get_rotate(self):
+        return api.MVector([math.degrees(i)
+                           for i in self._mfntrans.rotation()])
+
+    def get_scale(self):
+        return api.MVector(self._mfntrans.scale())
+
+    def set_pivot(self, point, space=api.MSpace.kWorld):
+        if isinstance(point, api.MVector):
+            point = api.MPoint(point)
+        self._mfntrans.setRotatePivot(point, space, True)
+        self._mfntrans.setScalePivot(point, space, True)
+
+    def set_rotate_pivot(self, point, space=api.MSpace.kWorld):
+        if isinstance(point, api.MVector):
+            point = api.MPoint(point)
+        self._mfntrans.setRotatePivot(point, space, True)
+
+    def set_scale_pivot(self, point, space=api.MSpace.kWorld):
+        if isinstance(point, api.MVector):
+            point = api.MPoint(point)
+        self._mfntrans.setScalePivot(point, space, True)
+
+
 if __name__ == '__main__':
-    pass
+    c = DagNode('pCube9')
+    trns = c.get_transform()
+    print trns.transforms.rotate
+
+
+    # cm = c._dagnode.transformationMatrix()
+    # pm = p._dagnode.transformationMatrix()
+
+    # m = cm - pm
+    # print type(m)
+    # c._dagnode.addChild(p.node)
+    # # p.translate.set(0, 0, 0)
+
+    # trans = p.get_transform()
+    # m = api.MTransformationMatrix(m)
+    # t = m.translation(api.MSpace.kWorld)
+    # trans._mfntrans.setTranslation(t, api.MSpace.kWorld)
