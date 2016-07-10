fbd5487a7f7ffa41328141985fd42e0ee5d38244
Marcus Albertsson <marcus.arubertoson@gmail.com>
2016-07-10 16:06:02 +0200

    Users must now excplicitly import modules instead of having everything available.
    Just a sane check. As the package grows it makes less sense to have
    everything available.

--- mampy/__init__.py
+++ mampy/__init__.py
@@ -34,19 +34,17 @@ __version__ = '0.0.4'
 __license__ = 'MIT'
 
 
-from mampy.utils import *
-from mampy.slist import *
-from mampy.node import *
-from mampy.comp import *
-from mampy.datatypes import *
-from mampy.exceptions import *
-from mampy.packages.mvp import Viewport, RenderGlobals
-from mampy.packages.pathlib import Path
-from mampy.packages import profilehooks
+# from mampy.utils import *
+# from mampy.slist import *
+# from mampy.node import *
+# from mampy.comp import *
+# from mampy.datatypes import *
+# from mampy.exceptions import *
 from mampy.api import (ls, selected, ordered_selection, get_node,
                        get_component, optionVar, get_active_mask)
 
 
+
 # Init
-option_var = OptionVar()
-mel_globals = MelGlobals()
+# option_var = OptionVar()
+# mel_globals = MelGlobals()
