880641590d09bd8e2d4312d6aba9be47b35d2bef
Marcus Arubertoson <marcus.arubertoson@gmail.com>
2016-08-20 01:02:05 +0200

    corrected dependencies

--- mampy/utils/__init__.py
+++ mampy/utils/__init__.py
@@ -4,7 +4,7 @@ from .history import History
 from .varlists import OptionVar, MelGlobals
 from .draggerctx import AbstractDraggerCtx
 from .masks import get_active_flags_in_mask, get_active_select_mode, SelectionMask
-from .dagnode import get_outliner_index
+from .dagnode import get_outliner_index, get_object_under_cursor, get_objects_in_view
 
 from .decorators import *
 
