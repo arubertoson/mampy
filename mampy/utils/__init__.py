9faee56f7ea14940f305d9a02f934f7529bbe21c
Marcus Arubertoson <marcus.arubertoson@gmail.com>
2016-08-30 10:37:33 +0200

    fixed SIngleton

--- mampy/utils/__init__.py
+++ mampy/utils/__init__.py
@@ -9,3 +9,11 @@ from .dagnode import get_outliner_index, get_object_under_cursor, get_objects_in
 from .decorators import *
 
 from mampy.packages import plugger, profilehooks, pathlib
+
+
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
