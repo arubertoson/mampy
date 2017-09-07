e8363344426f1c23e4e55ad07bf18674b8b170e2
Marcus Arubertoson <marcus.arubertoson@gmail.com>
2017-09-07 13:00:58 +0200

    Mod: Changed exceptions and warnings to Mampy base type
    Changed warnings and exceptions to mampy base type.
    
    Add: warning functions to wrap mayas cmds.warning and provide output in accordance with the more familiar warnings.warn(message, warningtype)

--- mampy/core/exceptions.py
+++ mampy/core/exceptions.py
@@ -1,30 +1,50 @@
+# -*- coding: utf-8 -*-
 """
+mampy.core.exception
+
+Exceptions and Warnings for Mampy and scripts utilizing Mampy.
 """
 import inspect
+from maya import cmds
 
 
-class AbstractException(Exception):
+class MampyException(Exception):
+
     def __str__(self):
         if not self.args:
             return inspect.getdoc(self)
-        return super(AbstractException, self).__str__()
+        return super(MampyException, self).__str__()
 
 
-class NothingSelected(AbstractException):
-    """Nothing is selected."""
-
-
-class InvalidSelection(AbstractException):
-    """An invalid selection was made."""
-
-
-class InvalidComponentSelection(AbstractException):
-    """Invalid Component object for operation"""
-
-
-class OrderedSelectionsNotSet(AbstractException):
+class OrderedSelectionsNotSet(MampyException):
     """Ordered selection not set in Maya preferences."""
 
 
-class ObjecetDoesNotExist(AbstractException):
+class ObjectDoesNotExist(MampyException):
     """Could not find wanted object in scene."""
+
+
+# Warnings
+
+
+class MampyWarning(Warning):
+    """Base warning for Mampy."""
+
+
+class NothingSelected(MampyWarning):
+    """Nothing is selected."""
+
+
+class InvalidSelection(MampyWarning):
+    """An invalid selection was made."""
+
+
+class InvalidComponentSelection(MampyWarning):
+    """Invalid Component object for operation"""
+
+
+def warning(message, warningtype):
+    frameinfo = inspect.getframeinfo(inspect.currentframe())
+    cmds.warning('{}: {}, at line {}, in "{}"'.format(
+        warningtype.__name__.replace('Warning', ''), message, frameinfo.lineno,
+        frameinfo.filename))
