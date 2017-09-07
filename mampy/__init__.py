dbe4bd547f3d6788c7e8dfe0bdeb16f53d966b02
Marcus Arubertoson <marcus.arubertoson@gmail.com>
2017-09-07 21:19:13 +0200

    Rem: unnecessary information that can be found in setup file

--- mampy/__init__.py
+++ mampy/__init__.py
@@ -5,9 +5,3 @@ Mampy is a Maya API adapter/wrapper, written in python. The maya api is overly
 cumbersome to use and not kind to the python sanity, this inspired mampy.
 """
 from mampy.api import *
-
-__author__ = 'Marcus Albertsson <marcus.arubertoson@gmail.com>'
-__copyright__ = 'Copyright 2015-2017 Marcus Albertsson'
-__url__ = 'http://github.com/arubertoson/mampy'
-__version__ = '0.2.0'
-__license__ = 'MIT'
