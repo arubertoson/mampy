9940e9ad90ad63d24cb74cbda4940bd3d33aab07
Marcus Arubertoson <marcus.arubertoson@gmail.com>
2017-09-07 14:13:30 +0200

    Mod: Api now supports new listadapter

--- mampy/__init__.py
+++ mampy/__init__.py
@@ -1,46 +1,13 @@
 """
-Mampy Maya library
+Mampy
 
-Mampy is a Maya API wrapper, written in python. The maya api is overly
-cumbersome to use and not kind to the python sanity, this inspired
-mampy.
-
-Basig Usage::
-
-    >>> import mampy
-    >>> slist = mampy.selected()
-    >>> component = slist.itercomps()
-    ['list of components']
-    >>> cmds.select(list(components), r=True)
-
-or to work with a Node::
-
-    >>> name = 'persp'
-    >>> camera = DagNode(name)
-    >>> camera.get_shape()
-    'perspShape'
-
-
-For more examples see documentation at <http://readthedocs.com>.
-
-:copyright: (c) 2016 Marcus Albertsson.
-:license: MIT, see LICENSE for more details.
-
+Mampy is a Maya API adapter/wrapper, written in python. The maya api is overly
+cumbersome to use and not kind to the python sanity, this inspired mampy.
 """
-import logging
-from mampy.api import (complist,
-                       multicomplist,
-                       daglist,
-                       dependlist,
-                       pluglist,
-                       get_single_index_component,
-                       get_node,
-                       get_depend_node,)
-
-log = logging.getLogger(__name__)
+from mampy.api import *
 
 __author__ = 'Marcus Albertsson <marcus.arubertoson@gmail.com>'
-__copyright__ = 'Copyright 2016 Marcus Albertsson'
-__url__ = 'http://github.com/arubertoson/maya-mampy'
+__copyright__ = 'Copyright 2015-2017 Marcus Albertsson'
+__url__ = 'http://github.com/arubertoson/mampy'
 __version__ = '0.2.0'
 __license__ = 'MIT'
--- mampy/api.py
+++ mampy/api.py
@@ -1,63 +1,14 @@
+# -*- coding: utf-8 -*-
 """
-This module implements the Mampy API.
+mampy.api
 
-:copyright: (c) 2016 by Marcus Albertsson
-:license: MIT, see LICENSE for more details.
+This module exposes internal functionality to the user.
 """
-from __future__ import absolute_import, unicode_literals
+from mampy.core.listadapters import ComponentList as complist
+from mampy.core.listadapters import DagpathList as daglist
+from mampy.core.listadapters import DependencyNodeList as deplist
+from mampy.core.listadapters import MultiComponentList as multicomplist
+from mampy.core.listadapters import PlugList as pluglist
 
-from mampy.core.dagnodes import Node, DependencyNode
-from mampy.core.components import SingleIndexComponent, get_component_from_string
-from mampy.core.selectionlist import (ComponentList, MultiComponentList, DagpathList,
-                                      DependencyList, PlugList)
-
-
-def _get_dagpath_list_from_type(list_object, *args, **kwargs):
-    if not args and not kwargs:
-        return list_object.from_selection()
-
-    elif args and isinstance(args[0], basestring) and not kwargs:
-        return list_object.from_name(args[0])
-
-    elif args and kwargs:
-        return list_object.from_ls(*args, **kwargs)
-
-    elif kwargs:
-        return list_object.from_ls(**kwargs)
-
-    elif args and isinstance(args[0], (tuple, list, set)):
-        return list_object(args[0])
-
-
-def multicomplist():
-    return MultiComponentList.from_selection()
-
-
-def complist(*args, **kwargs):
-    return _get_dagpath_list_from_type(ComponentList, *args, **kwargs)
-
-
-def daglist(*args, **kwargs):
-    return _get_dagpath_list_from_type(DagpathList, *args, **kwargs)
-
-
-def dependlist(*args, **kwargs):
-    return _get_dagpath_list_from_type(DependencyList, *args, **kwargs)
-
-
-def pluglist(name=None, *args, **kwargs):
-    return _get_dagpath_list_from_type(PlugList, *args, **kwargs)
-
-
-def get_depend_node(dagobject):
-    return DependencyNode(dagobject)
-
-
-def get_node(dagpath):
-    return Node(dagpath)
-
-
-def get_single_index_component(dagpath, object=None):
-    if isinstance(dagpath, basestring):
-        dagpath, object = get_component_from_string(dagpath)
-    return SingleIndexComponent(dagpath, object)
+from mampy.core import exceptions
+from mampy.core.exceptions import warning
